04a7acdfa4651a5a4dbe806110f560ca19e35f8d
GluterusMaximus <supkar1756@gmail.com>
2022-10-27 12:20:00 +0300

    Simple post creation and display

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <script>
+  import PostForm from '@/components/PostForm.vue'
+  import PostList from '@/components/PostList.vue'
 
+  export default {
+    components: {PostForm, PostList},
+    data(){
+      return {
+        posts: [
+          {id: 1, title: 'JavaScript', body: 'This post is about JavaScript'},
+          {id: 2, title: 'TypeScript', body: 'This post is about TypeScript'},
+          {id: 3, title: 'Vue', body: 'This post is about Vue'},
+        ],
+      }
+    },
+    methods: {
+      createPost(post) {
+        this.posts.push(post)
+      }
+    }
+  }
 </script>
 
 <template>
-
+  <div class="app">
+    <PostForm @create="createPost" />
+    <PostList :posts="posts"/>
+  </div>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.app {
+  padding: 20px;
+}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,54 @@
+<template lang="">
+  <form @submit.prevent>
+      <h4>Post Creation</h4>
+      <input class="input" v-model="post.title" type="text" placeholder="Title">
+      <input class="input" v-model="post.body" type="text" placeholder="Body">
+      <button class="btn" @click="createPost">Create</button>
+  </form> 
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        post: {
+          title: '',
+          body: ''
+        }
+      }
+    },
+    methods: {
+      createPost(event) {
+        this.post.id = Date.now()
+        this.$emit('create', this.post)
+        this.post = {
+          title: '',
+          body: ''
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
