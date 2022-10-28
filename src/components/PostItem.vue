233a8168789e60a6e6d17bf5cf5987db04414820
GluterusMaximus <supkar1756@gmail.com>
2022-10-28 10:43:00 +0300

    Post item component

---
+++ src/components/PostItem.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <div class="post" >
+    <div>Title: <strong>{{post.title}}</strong></div>
+    <div>Body: <strong>{{post.body}}</strong></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+  }
+</style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,18 @@
 <template lang="">
-  <div class="post" v-for="post in posts">
-    <div>Title: <strong>{{post.title}}</strong></div>
-    <div>Body: <strong>{{post.body}}</strong></div>
+  <div>
+    <h3>Recent posts</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post">
+    </post-item>
   </div>
 </template>
 
 <script>
+import PostItem from './PostItem.vue';
+
 export default {
+  components: {PostItem},
   props: {
     posts: {
       type: Array,
@@ -17,9 +23,4 @@ export default {
 </script>
 
 <style scoped>
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
 </style>
